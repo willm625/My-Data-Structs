cc90e006b4aa0dfafd588e708c6f1b5d0dde8835
William Cornejo <wcornej1@binghamton.edu>
2022-07-07 16:04:31 -0400

    functions

--- Min_Heap/Min_Heap.hpp
+++ Min_Heap/Min_Heap.hpp
@@ -7,11 +7,45 @@
 template<typename T> class Min_Heap{
 	private:
 		int * arr;
-		int sz;
+		size_t sz;
 	public:
+		//constructors
 		Min_Heap(){
-			arr = NULL;	
+			arr = NULL;
+			sz = 0;	
 		}
+		size_t size(){
+			return sz;
+		}
+		bool empty(){
+			return sz > 0 ? false : true;
+		}
+		//insert
+		void insert(const T& val){
+			if(arr == NULL){
+				arr = new T[1];
+				arr[0] = val;
+			}
+			else{
+				
+			}
+		}
+	
+		//remove
+
+		//access
+		int get_parent(int i){
+			return (i-1)/2;
+		}
+		int left(int i){
+			return (2*i) + 1;
+		}
+		int right(int i){
+			return (2*i) + 2;
+		}
+
+		//traversals
+		
 
 
 };
