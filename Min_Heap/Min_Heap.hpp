94619d27f938833edcb5deb40f0edebd1ea04042
William Cornejo <wcornej1@binghamton.edu>
2022-07-07 14:45:04 -0400

    started min heap

---
+++ Min_Heap/Min_Heap.hpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <iostream>
+#ifndef MIN_HEAP_HPP
+#define MIN_HEAP_HPP
+template<typename T> class Min_Heap{
+	private:
+		int * arr;
+		int sz;
+	public:
+		Min_Heap(){
+			arr = NULL;	
+		}
+
+
+};
+#endif
---
+++ Min_Heap/test.cpp
@@ -0,0 +1,7 @@
+#include "Min_Heap.hpp"
+
+int main(){
+	Min_Heap<int> my_heap;
+	std::cout << "HI\n";
+	return 0;
+}
