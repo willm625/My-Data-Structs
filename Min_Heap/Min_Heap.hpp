800022d3c1f4cd1b4ccdbd93767f4a588c249f71
William Cornejo <wcornej1@binghamton.edu>
2022-07-08 15:00:35 -0400

    insert

--- Min_Heap/Min_Heap.hpp
+++ Min_Heap/Min_Heap.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #ifndef MIN_HEAP_HPP
 #define MIN_HEAP_HPP
+//Min heap implemented by using an array;
+//dynamically resizes with inserts and removals
 template<typename T> class Min_Heap{
 	private:
 		int * arr;
@@ -20,18 +22,58 @@ template<typename T> class Min_Heap{
 		bool empty(){
 			return sz > 0 ? false : true;
 		}
+		void heapify(){
+			int index = sz - 1;
+			while(index > 0 && arr[get_parent(index)] > arr[index]){
+				//swap
+				int temp = arr[get_parent(index)];
+				arr[get_parent(index)] = arr[index];
+				arr[index] = temp;
+				index = get_parent(index);
+			}
+		}
+		void heapify(int index){
+			int l = left(index);
+			int r = right(index);
+			int smallest = index;
+		}
 		//insert
 		void insert(const T& val){
 			if(arr == NULL){
 				arr = new T[1];
 				arr[0] = val;
+				++sz;
 			}
 			else{
-				
+				++sz;
+				int index = sz - 1;
+				T* temp = new T[sz];
+				for(size_t i = 0; i < sz-1;i++){
+					temp[i] = arr[i];
+				}
+				temp[sz-1] = val;
+				delete arr;
+				arr = temp;
+				temp = NULL;
+				//now heapify
+				heapify();		
 			}
 		}
 	
 		//remove
+		T& top(){
+			return arr[0];
+		}
+		void pop(){
+			if(sz == 1){
+				sz--;
+				return arr[0];
+			}
+			T root = arr[0];
+			arr[0] = arr[sz-1];
+			sz--;
+			heapify(0);
+		}
 
 		//access
 		int get_parent(int i){
--- Min_Heap/test.cpp
+++ Min_Heap/test.cpp
@@ -3,5 +3,10 @@
 int main(){
 	Min_Heap<int> my_heap;
 	std::cout << "HI\n";
+	my_heap.insert(3);
+	my_heap.insert(20);
+	my_heap.insert(1);
+	std::cout << my_heap.size() << "\n";
+	std::cout << my_heap.top() << "\n";
 	return 0;
 }
